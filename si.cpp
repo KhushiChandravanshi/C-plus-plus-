fea27ea85ff9cd0a8e5f369c602951da88138640
KhushiChandravanshi <khushichandravanshi377@gmail.com>
2024-12-20 21:59:52 +0530

    d

---
+++ si.cpp
@@ -0,0 +1,384 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int p,r,n;
+//     float si;
+//     cout<<"Enter principal Amount :- ";
+//     cin>>p;
+//     cout<<"Enter rate of interest:- ";
+//     cin>>r;
+//     cout<<"Enter time :- ";
+//     cin>>n;
+//     si = (float) (p*r*n)/100;
+//     cout<<"\nsimple interest:- "<<si;
+// }
+// #include<iostream>preproceessor direactive
+// using namespace std;std file denote
+// int main()
+// {
+//     cout<<"hi ";
+// return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Khushi " <<"Chandravanshi"<<endl;
+//     return 0;
+// }
+// printing variables
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int age = 18;
+//     cout << age <<endl;
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int age = 25;
+//     cout<< sizeof(age) << endl;
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char grade = 'A';
+//     cout << grade << endl;
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float PI = 3.14f;
+//     cout << PI << endl;
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     bool isSafe = true;
+//     cout << isSafe << endl;
+//     return 0;
+// }
+// primitive
+// int 4 byte
+// char 1 byte
+// float 4 byte
+// bool 1 byte
+// double 8 byte
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     double price = 100.99;
+//     cout << price << endl;
+//     return 0;
+// }
+// implicit automatic by machine: conversion: small to large
+// explicit :casting
+// ascii :american standard code of interchange
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char Khushi = 'a';
+//     int Rudra = Khushi;
+//     cout << Rudra << endl;
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     double ram = 100.99;
+//     int shyam = (int)ram;
+//     cout << shyam << endl;
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int age;
+//     cout << "Enter the age : ";
+//     cin >> age;
+//     cout << "your age is : "<< age <<endl;
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int n = -45;//declartion
+//     if(n >= 0){
+//         cout << "n is positive\n";
+//     }else{
+//         cout << "n is negative\n";
+//     }
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int age;
+//     cout << "enter age : ";
+//     cin >> age;
+//     if(age >= 18){
+//         cout << "you can vote\n";
+//     }else{
+//         cout << "you can't vote\n";
+//     }
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout << "enter number : ";
+//     cin >> n;
+//     if( n%2 == 0){
+//         cout << "even";
+//         cout << "bye";
+//     }else{
+//         cout << "odd";
+//     }
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int marks;
+//     cout << "Enter marks : ";
+//     cin >> marks;
+//     if(marks >= 90){
+//         cout << "A\n";
+//     }else if(marks >= 80 && marks < 90){
+//         cout << "B\n";
+//     }else{
+//         cout << "C\n";
+//     }
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     char ch;
+//     cout << "Enter char : ";
+//     cin >> ch;
+//     if(ch >= 'a' && ch <= 'z'){
+//         cout << "lowercase\n";
+//     }else{
+//         cout << "uppercase\n";
+//     }
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     char ch;
+//     cout <<" Enter char : ";
+//     cin >> ch;
+//     if(ch >= 'A' && ch <= 'Z'){
+//         cout << "uppercase\n";
+//     }else{
+//         cout << "lowercase\n";
+//     }
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     char ch;
+//     cout <<"Enter char : ";
+//     cin >> ch;
+//     if(ch >= 65 && ch <= 90){
+//         cout << "uppercase\n";
+//     }else{
+//         cout << "lowercase\n";
+//     }
+// }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout << "enter no : ";
+//     cin >> n;
+//     cout << (n >= 0 ? "positive" : "negative") << endl;
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     int count = 1;
+//     while(count <= 10){
+//         cout << count << " ";
+//         count++;
+//     }
+//     cout << endl;
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n = 6;
+//     int count = 1;
+//     while(count <= n){
+//         cout << count << " ";
+//         count++;
+//     }
+//     cout << endl;
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n = 5;
+//     int i = 1;
+//     while(i <= n) {
+//         cout << i <<" ";
+//         i++;
+//     }
+//     cout << endl;
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n = 4;
+//     for(int i=1; i<=n; i++) {
+//         cout << i <<" ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n = 20;
+//     for(int i=1; i<=n; i = i+2) {
+//         cout << i <<" ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     int n = 4;
+//     int sum = 0;
+//     for(int i=1; i<=n; i++){
+//         sum += i;
+//     }
+//     cout << "sum = " << sum << endl;
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n = 5;
+//     int i = 1;
+//     int sum = 0;
+//     while( i<=n ){
+//          sum = sum + i;
+//          i++;
+//     }
+//     cout << sum << endl;
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n = 30;
+//     int i = 1;
+//     int sum = 0;
+//     while( i<=n ){
+//          sum = sum + i;
+//          i++;
+//     }
+//     cout << sum << endl;
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     int n = 10;
+//     int sum = 0;
+//     for(int i=1; i<=n; i++){
+//         sum += i;
+//         if (i==5){
+//             break;
+//         }
+//     }
+//     cout << "sum = " << sum << endl;
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n = 3;
+//     int oddSum = 0;
+//     for(int i=1; i<=n; i++){
+//         if(i%2 != 0){
+//             oddSum += i;
+//         }
+//     }
+//     cout << "odd Sum = "<< oddSum << endl;
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n = 10;
+//     int evensum=0;
+//     for(int i=1; i<=n; i++){
+//         if(i%2 ==0 ){
+//             evensum += i;
+//         }
+//     }
+//     cout << "even sum = "<< evensum <<endl;
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n = 3;
+//     int oddSum = 0;
+//     int i = 1; 
+//     while(i<=n){
+//         if(i%2! ==0 ){
+//             oddSum += i;
+//         }
+//        cout << oddSum <<""<< endl;;
+//     }
+    
+//     return 0;
+// }
+/*
+for loop
+for(initialisation;condition;updation){
+    work
+}
+*/
+/*
+while loop
+initialisation
+while(condition){
+    work
+    updation
+}
+*/
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n = 3;
+//     int oddSum = 0;
+//     int i = 1; 
+//     while (i <= n) {
+//         if (i % 2 != 0) {
+//             oddSum += i;
+//         }
+//         i++;  // Increment i to avoid infinite loop
+//     }
+//     cout << "The sum of odd numbers up to " << oddSum << " is: " << oddSum << endl;
+    
+//     return 0;
+// }
